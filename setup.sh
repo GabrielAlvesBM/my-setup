36f0272ec15c65db352b06a21788d0a5cb23d6fd
Gabriel Alves <gabrielalves6002@gmail.com>
2025-07-11 23:03:26 -0300

    feat: zsh customs

--- setup.sh
+++ setup.sh
@@ -18,7 +18,7 @@ sudo pacman -S --needed --noconfirm pipewire pipewire-alsa pipewire-jack pipewir
   mesa lib32-mesa vulkan-intel vulkan-radeon ark kio-admin polkit-kde-agent qt5-wayland qt6-wayland \
   xdg-desktop-portal-hyprland xdg-desktop-portal-gtk xdg-desktop-portal-wlr xdg-user-dirs-gtk \
   hyprland hyprlock hypridle hyprcursor hyprpaper hyprpicker hyprshot waybar kitty rofi-wayland dolphin \
-  zsh vi vim \
+  zsh vi vim curl \
   dolphin-plugins vivaldi dunst cliphist mpv pavucontrol ttf-font-awesome ttf-jetbrains-mono-nerd \
   ttf-opensans noto-fonts noto-fonts-emoji ttf-droid ttf-roboto ttf-fira-code ttf-fira-mono ttf-firacode-nerd \
   otf-font-awesome
@@ -73,6 +73,44 @@ rsync -a --delete "$DOTFILES_DIR/Pictures/wallpapers/" "$PICTURES_DIR/wallpapers
 if [[ "$SHELL" != "/bin/zsh" ]]; then
   echo "Setting ZSH as default shell..."
   chsh -s /bin/zsh
+fi
+
+echo "Installing Oh My Zsh..."
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+else
+  echo "Oh My Zsh already installed."
+fi
+
+echo "Installing zsh plugins..."
+ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
+
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
+fi
+
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
+fi
+
+echo "Installing Powerlevel10k theme..."
+if [ ! -d "$ZSH_CUSTOM/themes/powerlevel10k" ]; then
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$ZSH_CUSTOM/themes/powerlevel10k"
+fi
+
+echo "Linking Zsh configuration..."
+if [ -e "$HOME/.zshrc" ] || [ -L "$HOME/.zshrc" ]; then
+  echo "Backing up existing .zshrc to .zshrc.bak"
+  mv "$HOME/.zshrc" "$HOME/.zshrc.bak"
+fi
+ln -sf "$DOTFILES_DIR/.config/.zshrc" "$HOME/.zshrc"
+
+if [ -f "$DOTFILES_DIR/.config/.p10k.zsh" ]; then
+  if [ -e "$HOME/.p10k.zsh" ] || [ -L "$HOME/.p10k.zsh" ]; then
+    echo "Backing up existing .p10k.zsh to .p10k.zsh.bak"
+    mv "$HOME/.p10k.zsh" "$HOME/.p10k.zsh.bak"
+  fi
+  ln -sf "$DOTFILES_DIR/.config/.p10k.zsh" "$HOME/.p10k.zsh"
 fi
 
 echo "Setup completed successfully!"
